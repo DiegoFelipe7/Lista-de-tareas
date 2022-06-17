a3487c2643adc17465ca179b12a0fa0750ba29ab
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-06-17 14:01:48 -0500

    Cambio de template a fragmentos

--- js/tareamodificado.js
+++ js/tareamodificado.js
@@ -1,4 +1,5 @@
 const url = "http://localhost:8080/tareas";
+const url2 = "http://localhost:8080/subtareas";
 fetch(url)
   .then((response) => response.json())
   .then((data) => datos(data))
@@ -8,6 +9,7 @@ const datos = (data) => {
   const fragment = document.createDocumentFragment();
 
   data.forEach((element) => {
+    console.log(element);
     //Div
     const div = document.createElement("div");
     div.classList.add(
@@ -36,7 +38,7 @@ const datos = (data) => {
 
     //button eliminar
     const button_eliminar = document.createElement("button");
-    button_eliminar.classList.add("btn-close", "text-dark");
+    button_eliminar.classList.add("eliminar_tarea", "btn-close", "text-dark");
     button_eliminar.dataset.id = element.id;
     //fin button eliminar
 
@@ -82,9 +84,15 @@ const datos = (data) => {
 
     //button guardar
     const button_guardar = document.createElement("button");
-    button_guardar.classList.add("btn", "btn-dark", "fw-bold");
+    button_guardar.classList.add(
+      "guardar_subtarea",
+      "btn",
+      "btn-dark",
+      "fw-bold"
+    );
     button_guardar.type = "button";
     button_guardar.textContent = "Guardar";
+    button_guardar.dataset.id = element.id;
     //fin button
     //contendor de la tabla
     const container_tabla = document.createElement("div");
@@ -113,32 +121,45 @@ const datos = (data) => {
     //fin
 
     //table-body
-    const tbody = document.createElement("tbody");
-    for (const key in data.subtareas) {
+    const tbodytabla = document.createElement("tbody");
+    for (const key in element.subtarea) {
       //tr body
-      const trBody = document.createElement("tr");
+      const tr = document.createElement("tr");
+      //
       const tdNombre = document.createElement("td");
-      tdNombre.innerHTML = data.subtareas[key].nombre;
+      tdNombre.innerHTML = element.subtarea[key].nombre;
       const tdEstado = document.createElement("td");
-      tdEstado.innerHTML = data.subtareas[key].estado;
+      tdEstado.innerHTML = element.subtarea[key].estado;
 
       const tdAcciones = document.createElement("td");
       const btn_editar = document.createElement("button");
-      button_editar.classList.add("btn", "btn-dark");
-      button_editar.textContent = "U";
-      button_editar.dataset.id = data.subtareas[key].id;
-      button_editar.dataset.id = data.subtareas[key].nombre;
+      btn_editar.classList.add("actualizar_sub", "btn", "btn-dark");
+      btn_editar.textContent = "U";
+      btn_editar.title = "Editar tarea";
+      btn_editar.dataset.id = element.subtarea[key].id;
+      btn_editar.dataset.nombre = element.subtarea[key].nombre;
 
       const btn_eliminar = document.createElement("button");
-      button_editar.classList.add("btn", "btn-dark");
-      button_editar.textContent = "U";
-      button_editar.dataset.id = data.subtareas[key].id;
+      btn_eliminar.classList.add("eliminar_sub", "btn", "btn-danger", "ms-1");
+      btn_eliminar.textContent = "D";
+      btn_editar.title = "Eliminar tarea";
+      btn_eliminar.dataset.id = element.subtarea[key].id;
 
-      tdAcciones.append(btn_editar, btn_eliminar);
+      const btn_completado = document.createElement("button");
+      btn_completado.classList.add(
+        "completado_sub",
+        "btn",
+        "btn-success",
+        "ms-1"
+      );
+      btn_completado.textContent = "C";
+      btn_editar.title = "Completando tarea";
+      btn_completado.dataset.id = element.subtarea[key].id;
 
-      trBody.append(tdNombre, tdEstado, tdAcciones);
-      tbody.append(trBody);
-      table.append(tbody);
+      tdAcciones.append(btn_editar, btn_eliminar, btn_completado);
+
+      tr.append(tdNombre, tdEstado, tdAcciones);
+      tbodytabla.append(tr);
     }
 
     row.append(col_1);
@@ -150,74 +171,114 @@ const datos = (data) => {
     card.append(titulo, card_body);
     div.append(hr, row, card, container_tabla);
     tr.append(thSub, thEstado, thAcciones);
-    table.append(tr);
+    table.append(tr, tbodytabla);
     container_tabla.append(h3, table);
     fragment.append(div);
   });
   container.append(fragment);
 };
 
-/*  <div class="w-100 border shadow rounded p-5 mt-2 bg-white">
-              <hr class="bg-dark" />
-              <div class="row justify-content-end">
-                <div class="col-1">
-                  <button
-                    id="eliminar_tarea"
-                    name="eliminar_tarea"
-                    title="Eliminar tarea"
-                    class="eliminar_tarea btn-close text-dark"
-                    aria-label="Close"
-                  ></button>
-                </div>
-              </div>---aqui voy
+document.getElementById("guardar_tarea").addEventListener("click", (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById("nombre").value;
+  if (nombre.trim() === "") {
+    Swal.fire("Ingresa una tarea");
+    return;
+  }
+  let config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre: nombre,
+    }),
+  };
+  fetch(url, config)
+    .then((response) => response.json())
+    .then((data) => {
+      Swal.fire("Registro exito!", "Tarea registrada");
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
+    })
+    .catch((error) => console.log(error));
+});
 
+document.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.matches(".eliminar_tarea")) {
+    Swal.fire({
+      title: "¿Estas seguro de eliminar esta tarea?",
+      showDenyButton: true,
+      confirmButtonText: "Si",
+    }).then((result) => {
+      let configuracion = {
+        method: "DELETE",
+      };
+      if (result.isConfirmed) {
+        Swal.fire("Eliminado!", "", "success");
+        fetch(`${url}/del/${e.target.dataset.id}`, configuracion)
+          .then((response) => response.json())
+          .then((data) => location.reload())
+          .catch((err) => {
+            location.reload();
+          });
+      }
+    });
+  }
 
-
-               <div class="card mt-2">
-                <h5 id="titulo-tarea" class="card-header"></h5>
-                <div class="card-body"> --
-                  <form id="form-guardar-tarea" method="POST">
-                    <div class="row">
-                      <div class="col-8">
-                        <input
-                          id="subtarea"
-                          name="subtarea"
-                          class="subtarea form-control w-100"
-                          placeholder="Ingresa una subtareas "
-                        />
-                      </div>
-                      <div class="col-4">
-                        <button
-                          id="guardar_subtarea"
-                          name="guardar_subtarea"
-                          class="guardar_subtarea btn btn-outline-secondary"
-                        >
-                          Guardar
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>--aqui
-              
-
-                <hr />
-              <div class="w-100">
-                <h3 class="text-dark text-center fw-bold">
-                  Subtareas 📋<samp></samp>
-                </h3>
-                <table id="super-tabla" class="table table-striped">
-                  <thead>
-                    <tr>
-                      <th>Actividad</th>
-                      <th>Estado</th>
-                      <th>Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody id="tbody-final"></tbody>
-                </table>
-              </div>
-            </div>
-              
-              
-              */
+  if (e.target.matches(".guardar_subtarea")) {
+    let subtarea = document.getElementById("nombre_subtarea").value;
+    if (subtarea.trim() === "") {
+      Swal.fire("Ingresa subtarea");
+      return;
+    }
+    console.log(e.target.dataset.id);
+    let config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        estado: "incompleta",
+        nombre: subtarea,
+        id_subtareas: {
+          id: e.target.dataset.id,
+        },
+      }),
+    };
+    fetch(url2, config)
+      .then((response) => response.json())
+      .then((data) => {
+        Swal.fire("Registro exito!", "Tarea registrada");
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      })
+      .catch((error) => console.log(error));
+  }
+  if (e.target.matches(".eliminar_sub")) {
+    Swal.fire({
+      title: "¿Estas eliminar esta sub tarea?",
+      showDenyButton: true,
+      confirmButtonText: "Si",
+    }).then((result) => {
+      let configuracion = {
+        method: "DELETE",
+      };
+      if (result.isConfirmed) {
+        Swal.fire("Eliminado!", "", "success");
+        fetch(`${url2}/del/${e.target.dataset.id}`, configuracion)
+          .then((response) => response.json())
+          .then((data) => location.reload())
+          .catch((err) => {
+            location.reload();
+          });
+      }
+    });
+  }
+  if (e.target.matches("."))
+    if (e.target.matches(".actualizar_sub")) {
+    }
+});
